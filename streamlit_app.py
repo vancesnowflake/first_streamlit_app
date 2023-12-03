0e65977cbd14082a998b3f3cc96315f26f92c76a
vancesnowflake <152816150+vancesnowflake@users.noreply.github.com>
2023-12-03 15:21:33 -0700

    Update streamlit_app.py
    added icons
    added smoothie header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard Boiled Free Range Egg')
-streamlit.text('Pig Slop')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard Boiled Free Range Egg')
+streamlit.text('🍞🥑 Pig Slop ')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
